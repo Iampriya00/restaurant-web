b0736c72b149980f8d7dc509d293b7c7b64a7108
Priya Chakraborty <priyachakrab01411@gmail.com>
2024-02-16 22:01:23 +0530

    update home page

--- src/components/Layouts/Button.js
+++ src/components/Layouts/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = (props) => {
   return (
     <div>
-      <button className="px-6 py-1 transition-all border-2 rounded-full  border-brightColor text-brightColor hover:bg-brightColor hover:text-white">
+      <button className="px-6 py-1 transition-all border-2 rounded-full border-brightColor text-brightColor hover:bg-stone-950 hover:text-white">
         {props.title}
       </button>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,34 @@
 import React from "react";
 import Button from "../components/Layouts/Button.js";
+import HeroImg from '../img/ban.png'
+
 import Layout from '../components/Layouts/Layout';
 const Home = () => {
   return (
     <Layout>
-      
-    <div className=" min-h-screen flex justify-between items-center lg:px-32 px-5 bg-[url('./img/ban.png')] bg-no-repeat ">
-      <div className="w-full space-y-5 lg:w-2/3">
-        <h1 className="text-6xl font-semibold text-backgroundColor">
-          Elevate Your Inner Foodie with Every Bite.
-        </h1>
-        <p className=" text-backgroundColor">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis et qui,
-          maxime assumenda repellat corrupti officia dolorum delectus labore
-          deleniti?
-        </p>
-        <div className=" lg:pl-44">
-          <Button title="Order Now" />
+      <div className="container min-h-screen mx-auto">
+        <div className="grid grid-cols-2 gap-2 pt-10 px-7">
+          <div className="w-full gap-4 space-y-9 lg:w-4/5">
+            <h1 className="pt-40 pl-10 text-5xl font-semibold text-backgroundColor">
+              Elevate Your Inner Foodie with Every Bite.
+            </h1>
+            <p className="pl-10 text-backgroundColor">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis et qui,
+              maxime assumenda repellat corrupti officia dolorum delectus labore
+              deleniti?
+            </p>
+            <div className=" lg:pl-44">
+              <Button title="Order Now" />
+            </div>
+          </div>
+          <div align='right' margin='100px' padding-top='50px' padding-right='60px'>
+            <img src={HeroImg} alt="img" height='100%' width='100%'/>
+          </div>
         </div>
+
+
       </div>
-    </div>
+
     </Layout>
   );
 };
